512c15bb84e221958101eacaf5cb1d6dd9f6e577
p4anc <toni7902@hotmail.com>
2019-07-12 00:34:17 +0200

    hola y adios

--- hello.py
+++ hello.py
@@ -1,2 +1,13 @@
-venv/
-.vscode/
+from flask import Flask
+app = Flask(__name__)
+
+#decorador
+@app.route("/")
+
+def hello_world():
+    return "Hola, mundo"
+    
+@app.route("/adios")
+
+def bye():
+    return "adios, mundo cruel"
